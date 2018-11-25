120b45e4d98d625f3cded5b4befe371cd3b3a5cc
Daniel Pagan <dpagan@gatech.edu>
2018-11-25 16:10:22 +0000

    Internals of sha2 done

--- hash.c
+++ hash.c
@@ -1,7 +1,9 @@
 #include "hash.h"
 
 //hash value
-unsigned static int h[8];
+static unsigned int h[8];
+//message schedule array
+static unsigned int w[64];
 //sha256 round constant table
 unsigned static int k[64] = {
 0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
@@ -15,22 +17,28 @@ unsigned static int k[64] = {
 };
 
 /**
- * Runs sha256 on the data.
- * @param data - array of data to hash
- * @param size - size of the data array
- * @param buffer - 512 bit aligned array for the data (rounded up)
- * @return - hashed value
+ * A simple function to rotate an unsigned integer right by n.
+ * @param a - integer to rotate
+ * @param n - number of spaces to rotate
+ * @return - the rotated value of a
  */
-void sha256(unsigned int* data, int size, unsigned int *buffer, unsigned int *hash) {
-    init(data, size, buffer);
-
+static unsigned int rotright(unsigned int a, int n) {
+    unsigned int y = a >> n;
+    unsigned int z = a << (sizeof(unsigned int) - n);
+    return y | z;
 }
 
 
 /**
- * Initialize the hash values and do pre-processing
+ * Initialize the hash values and do pre-processing in order to set up for the
+ * internal sha256 function.
+ *
+ * @param data - the user data that is stored with integers
+ * @param size - the bitsize of the data
+ * @param buffer - the buffer that will contain the 512 bit aligned data with
+ * padding
  */
-static void init(unsigned int *data, int size, unsigned int *buffer) {
+static int init(unsigned int *data, int size, unsigned int *buffer) {
     h[0] = 0x6a09e667;
     h[1] = 0xbb67ae85;
     h[2] = 0x3c6ef372;
@@ -40,10 +48,101 @@ static void init(unsigned int *data, int size, unsigned int *buffer) {
     h[6] = 0x1f83d9ab;
     h[7] = 0x5be0cd19;
 
+    //TODO: determine padding
+
     //populate the buffer
     int i;
-    for (i = 0; i < size; i++) {
+    for (i = 0; i < size / 32; i ++) {
         buffer[i] = data[i];
     }
 
+    //TODO: populate the buffer's padding
+
+}
+
+/**
+ * The internal core operation of the hash function, which includes the
+ * compression function, and all of the linear and non-linear operations that
+ * provide cryptographic security.
+ *
+ * @param buffer - the data to hash in an array of integers
+ * @param bufsize - the size of the buffer's array not the number of bits of
+ * data
+ */
+static void sha256internal(unsigned int *buffer, int bufsize) {
+    //operate on each 512 bit chunk
+    //now chunks will be accessed like buffer[chunkid + (0 thru 15)]
+    for (int chunkind = 0; chunkind < bufsize; chunkind += 16) {
+
+        //initialize message schedule array
+        for (int i = 0; i < 16; i++) {
+            w[i] = buffer[chunkind + i];
+        }
+        for (int i = 16; i < 64; i++) {
+            unsigned int s0 = rotright(w[i-15], 7) ^ rotright(w[i-15], 18) ^ (w[i-15] >> 3);
+            unsigned int s1 = rotright(w[i-2], 17) ^ rotright(w[i-2], 19) ^ (w[i-2] >> 10);
+            w[i] = w[i-16] + s0 + w[i-7] + s1;
+        }
+
+        //initialize working variables to current hash value
+        unsigned int a = h[0];
+        unsigned int b = h[1];
+        unsigned int c = h[2];
+        unsigned int d = h[3];
+        unsigned int e = h[4];
+        unsigned int f = h[5];
+        unsigned int g = h[6];
+        unsigned int h = h[7];
+
+        //compression function
+        for (int i = 0; i < 64; i++) {
+            unsigned int S0, S1, ch, temp1, temp2, maj;
+            S1 = rotright(e, 6) ^ rotright(e, 11) ^ rotright(e, 25);
+            ch = (e & f) ^ ((~e) & g);
+            temp1 = h + S1 + ch + k[i] + w[i];
+            S0 = rotright(a, 2) ^ rotright(a, 13) ^ rotright(a, 22);
+            maj = (a & b) ^ (a & c) ^ (b & c);
+            temp2 = S0 + maj;
+            h = g;
+            g = f;
+            f = e;
+            e = d + temp1;
+            d = c;
+            c = b;
+            b = a;
+            a = temp1 + temp2;
+        }
+        //update the current hash value for the next iteration
+        h[0] = h[0] + a;
+        h[1] = h[1] + b;
+        h[2] = h[2] + c;
+        h[3] = h[3] + d;
+        h[4] = h[4] + e;
+        h[5] = h[5] + f;
+        h[6] = h[6] + g;
+        h[7] = h[7] + h;
+
+    }
+}
+
+/**
+ * Sets the user's hash value to the computed value.
+ */
+static void sha256final(unsigned int *hash) {
+    for (int i = 0; i < 8; i++) {
+        hash[i] = h[i];
+    }
+}
+
+/**
+ * Runs sha256 on the data.
+ * @param data - array of data to hash
+ * @param size - bit size of the data
+ * @param buffer - 512 bit aligned (rounded up) buffer to hold the data
+ * @return - hashed value
+ */
+void sha256(unsigned int* data, int size, unsigned int *buffer, unsigned int *hash) {
+    int bufsize = init(data, size, buffer);
+    sha256internal(buffer, bufsize);
+    sha256final(hash);
 }
--- hash.h
+++ hash.h
@@ -1,6 +1,6 @@
 #ifndef HASH_H
 #define HASH_H
 
-void sha256(unsigned int*, int, unsigned int*, unsigned int*);
+void sha256(unsigned int *data, int size, unsigned int *buffer, unsigned int *hash);
 
 #endif
